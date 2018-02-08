488e03f3c59555ad3592c54134b1ecfcca790c47
Rafaela Araujo <rafaela.campos.araujo@gmail.com>
2018-02-08 08:48:59 -0200

    fix travis

---
+++ app/src/test/java/com/bestsellers/bestSellers/bestSellers/BestSellersPresenterTest.kt
@@ -0,0 +1,12 @@
+package com.bestsellers.bestSellers.bestSellers
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Created by rafaela.araujo on 08/02/18.
+ */
+
+@RunWith(JUnit4::class)
+class BestSellersPresenterTest {
+}
